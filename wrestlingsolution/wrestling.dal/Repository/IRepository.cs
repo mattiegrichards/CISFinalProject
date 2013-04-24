924bbd7b5064afe176c638d60f2f59d0db9bffd5
Shintsu <shintsu1945@gmail.com>
2013-04-23 22:24:53 -0400

    Added repos to new project

---
+++ wrestlingsolution/wrestling.dal/Repository/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wrestling.dal.Repository
+{
+    interface IRepository<T>
+    {
+        T getById(T object2Add);
+        T[] getAll();
+        void add(T object2Add);
+        void update(T object2Update);
+        void remove(T object2Remove);
+        IQueryable<T> query(Expression<Func<T, bool>> filter); 
+    }
+}
---
+++ wrestlingsolution/wrestling.dal/Repository/MatchRepo.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wrestling.dal.Repository
+{
+    interface MatchRepo : IRepository<Match>
+    {
+        private student10Entities _context = null;
+
+        public MatchRepo() {
+        _context = new student10Entities();    
+        }
+
+        public Match getById(Match match2get)
+        {
+            return _context.Matches.Find(match2get.Match_ID);
+        }
+
+        public Match[] getAll()
+        {
+            return _context.Matches.ToArray();
+        }
+
+        public void add(Match Match2add)
+        {
+            _context.Matches.Add(Match2add);
+            _context.SaveChanges();
+        }
+
+        public void update(Match Match2update)
+        {
+            _context.Entry(Match2update).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+
+        public void remove(Match Match2remove)
+        {
+            _context.Matches.Remove(Match2remove);
+            _context.SaveChanges();
+        }
+
+        public IQueryable<Match> query(System.Linq.Expressions.Expression<Func<Match, bool>> filter)
+        {
+            return _context.Matches.Where(filter);
+        }
+    }
+}
